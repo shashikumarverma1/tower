b620f010b355911ea834838e7ac11661156bf0e3
shashi kumar verma <shashik.verma@cloudactivelabs.in>
2024-03-23 19:44:06 +0530

    format done

--- src/App.js
+++ src/App.js
@@ -12,68 +12,57 @@ function App() {
   const [show, setShow] = useState(false)
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
+  }
   const handaleclick = (num) => {
     console.log(num)
-    if(show){
+    if (show) {
       alert("play again")
     }
-    if(num == 1){
+    if (num == 1) {
       setShow(true)
-    
+
       console.log("u loose")
       setGameOverMassage('Game over')
-    }else{
+    } else {
       let digits = gameLevel.split('');
       let shuffledDigits = shuffleArray(digits).join('');
       // console.log(shuffledDigits);
-      setArr([shuffledDigits , ...arr  ])
+      setArr([shuffledDigits, ...arr])
     }
   }
-const Play_Game=()=>{
-  setShow(false)
-  setGameOverMassage('')
-  // EASY
-  if(gameLevel=='0001'){setArr(['0100' , '0010', '0100'])}
-  // HARD
-  if(gameLevel=='0111'){ setArr(['1011' , '1101', '0111'])}
-  // MEDIUM
-  if(gameLevel=='0011'){ setArr(['1001', '1010', '0110'])}
-  
-}
+  const Play_Game = () => {
+    setShow(false)
+    setGameOverMassage('')
+    // EASY
+    if (gameLevel == '0001') { setArr(['0100', '0010', '0100']) }
+    // HARD
+    if (gameLevel == '0111') { setArr(['1011', '1101', '0111']) }
+    // MEDIUM
+    if (gameLevel == '0011') { setArr(['1001', '1010', '0110']) }
+
+  }
 
   return (
     <div style={{ color: "white" }} className='app'>
-      <div
-        
-        style={{height:100, width:1200, 
-        '@media (max-width: 786px)': {
-        
-            backgroundColor: 'aqua', // Change background color for screens less than 786px
-          
-        },
-        '@media (min-width: 806px)': {
-        
-          backgroundColor: 'red', // Change background color for screens less than 786px
-        
-      },
-        
-        }}></div>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10vh',
 
-      
-       }}>
-        
-        <div style={{ width: '30vw', height: '83vh', backgroundColor: '#202020' ,
-      
-      }}>
-        <div style={{ display: 'flex', width: "90%", justifyContent: "space-around", textAlign: "center", margin:20 , backgroundColor:"#141414" , 
+      <div style={{
+        display: 'flex', justifyContent: 'center', marginTop: '10vh',
+
 
       }}>
+
+        <div style={{
+          width: '30vw', height: '83vh', backgroundColor: '#202020',
+
+        }}>
+          <div style={{
+            display: 'flex', width: "90%", justifyContent: "space-around", textAlign: "center", margin: 20, backgroundColor: "#141414",
+
+          }}>
             <p style={{
               display: 'flex',
               alignItems: 'center',
@@ -81,13 +70,14 @@ const Play_Game=()=>{
               height: '100%',
               textTransform: 'uppercase',
               backgroundColor: "#141414",
-              color: gameLevel === '0111' ? "#ffffde" :'#999999',
-              cursor:'pointer'
-             
-            }} onClick={() =>{
-              setArr(['1011' , '1101', '0111'])
-              setGameLevel('0111')}}>HARD</p>
-              {/* 😊 😡 */}
+              color: gameLevel === '0111' ? "#ffffde" : '#999999',
+              cursor: 'pointer'
+
+            }} onClick={() => {
+              setArr(['1011', '1101', '0111'])
+              setGameLevel('0111')
+            }}>HARD</p>
+            {/* 😊 😡 */}
             <p style={{
               display: 'flex',
               alignItems: 'center',
@@ -95,12 +85,13 @@ const Play_Game=()=>{
               height: '100%',
               textTransform: 'uppercase',
               backgroundColor: "#141414",
-              color: gameLevel === '0011' ? "#ffffde" :'#999999',
-              cursor:'pointer'
-              
+              color: gameLevel === '0011' ? "#ffffde" : '#999999',
+              cursor: 'pointer'
+
             }} onClick={() => {
               setArr(['1001', '1010', '0110'])
-              setGameLevel('0011')}}>MEDIUM</p>
+              setGameLevel('0011')
+            }}>MEDIUM</p>
             <p style={{
               display: 'flex',
               alignItems: 'center',
@@ -108,64 +99,66 @@ const Play_Game=()=>{
               height: '100%',
               textTransform: 'uppercase',
               backgroundColor: "#141414",
-              color: gameLevel === '0001' ? "#ffffde" :'#999999' ,
-              cursor:'pointer'
-            
-            }} 
-            onClick={() => {
-              setArr(['0100' , '0010', '0100'])
-              setGameLevel('0001')}}
-            
-              >EASY</p>
+              color: gameLevel === '0001' ? "#ffffde" : '#999999',
+              cursor: 'pointer'
+
+            }}
+              onClick={() => {
+                setArr(['0100', '0010', '0100'])
+                setGameLevel('0001')
+              }}
+
+            >EASY</p>
           </div>
           <div style={{
-         display:"flex",
-         justifyContent:"center",
-         alignItems:"center", 
-         marginTop:-10,
-         cursor:'pointer'
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: -10,
+            cursor: 'pointer'
           }}>
-            <h5 style={{   position: 'relative',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            margin:10,
-            width: '90%',
-            height: 60,
-            // paddingTop:20,
-            // paddingBottom:20,
-            borderRadius:7,
-            fontSize:22,
-            // color: 'red',
-            backgroundColor: "red",
-            cursor:'pointer'
-            // backgroundColor: 'linear-gradient(225deg, #77FFA8 0%, #FFE081 33%, #FFAD8E 68%, #A592FF 100%)'
-          }}
-          onClick={Play_Game}
+            <h5 style={{
+              position: 'relative',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              margin: 10,
+              width: '90%',
+              height: 60,
+              // paddingTop:20,
+              // paddingBottom:20,
+              borderRadius: 7,
+              fontSize: 22,
+              // color: 'red',
+              backgroundColor: "red",
+              cursor: 'pointer'
+              // backgroundColor: 'linear-gradient(225deg, #77FFA8 0%, #FFE081 33%, #FFAD8E 68%, #A592FF 100%)'
+            }}
+              onClick={Play_Game}
             >
               PLAY
             </h5>
           </div>
-        <div style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
-        <div style={{ display: "flex", justifyContent: "space-between", padding: 5, border: "3px solid rgb(68, 68, 68)", height: 60, borderRadius: 6, alignItems: "center", margin: 5 , width:'90%' }}>
-            <div>
-              <div><button style={button} onClick={() => setStakeValue(0.000001)}>-</button></div>
-              <div><button style={button} onClick={() => {
-                setStakeValue('0.00000001')
-              }}>MIN</button></div>
-            </div>
-            <div>
-              <p style={{ color: "#ffffff", fontWeight: "700", lineHeight: .5, textAlign: "center" }}>{stakeValue}</p>
-              <p style={{ color: "rgb(153, 153, 153)", fontWeight: "700", lineHeight: .5, textAlign: "center" }}>$0.001 STAKE</p>
-            </div>
-            <div>
-              <div> <button style={button} onClick={() => {
-                setStakeValue('0.00000110')
-              }}>+</button></div>
-              <div> <button style={button} onClick={() => setStakeValue('0.00000110')}>MAX</button></div>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", padding: 5, border: "3px solid rgb(68, 68, 68)", height: 60, borderRadius: 6, alignItems: "center", margin: 5, width: '90%' }}>
+              <div>
+                <div><button style={button} onClick={() => setStakeValue(0.000001)}>-</button></div>
+                <div><button style={button} onClick={() => {
+                  setStakeValue('0.00000001')
+                }}>MIN</button></div>
+              </div>
+              <div>
+                <p style={{ color: "#ffffff", fontWeight: "700", lineHeight: .5, textAlign: "center" }}>{stakeValue}</p>
+                <p style={{ color: "rgb(153, 153, 153)", fontWeight: "700", lineHeight: .5, textAlign: "center" }}>$0.001 STAKE</p>
+              </div>
+              <div>
+                <div> <button style={button} onClick={() => {
+                  setStakeValue('0.00000110')
+                }}>+</button></div>
+                <div> <button style={button} onClick={() => setStakeValue('0.00000110')}>MAX</button></div>
+              </div>
             </div>
           </div>
-        </div>
         </div>
         <div style={{ width: '40vw', height: '80vh', backgroundColor: 'grey', display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
           <div style={{ backgroundColor: '#202020', width: '90%', height: '80%', }}>
@@ -173,24 +166,24 @@ const Play_Game=()=>{
 
             </div>
             {
-              arr.slice(0,4).map((e, i) => {
+              arr.slice(0, 4).map((e, i) => {
                 return (
                   <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <button style={(i + 1) % 3 === 0 ? grad1 : (i + 1) % 4 === 0 ? greenbox : blackbox1}
                       onClick={() => handaleclick(arr[i][0])}
-                    >{(((i + 1) % 3 ===0) && show) && (arr[i][0] == 0 ? '😊' : '😡') }{(i + 1) % 4 ===0 && (arr[i][0] == 0 ? '😊' : '😡' ) }</button>
-                    <button style={(i + 1) % 3 === 0 ? grad2 : (i + 1) % 4 === 0 ? greenbox : blackbox1} onClick={() => handaleclick(arr[i][1])}>{(((i + 1) % 3 ===0) && show) && (arr[i][1] == 0 ? '😊' : '😡')  }{(i + 1) % 4 ===0 && (arr[i][1] == 0 ? '😊' : '😡' ) }</button>
-                    <button style={(i + 1) % 3 === 0 ? grad3 : (i + 1) % 4 === 0 ? greenbox : blackbox1} onClick={() => handaleclick(arr[i][2])}>{(((i + 1) % 3 ===0) && show) && (arr[i][2] == 0 ? '😊' : '😡')  }{(i + 1) % 4 ===0 && (arr[i][2] == 0 ? '😊' : '😡' ) }</button>
-                    <button style={(i + 1) % 3 === 0 ? grad4 : (i + 1) % 4 === 0 ? greenbox : blackbox1} 
-                    onClick={() => handaleclick(arr[i][3])}
-                    >{(((i + 1) % 3 ===0) && show) && (arr[i][3] == 0 ? '😊' : '😡')  }{(i + 1) % 4 ===0 && (arr[i][3] == 0 ? '😊' : '😡' ) }</button>
+                    >{(((i + 1) % 3 === 0) && show) && (arr[i][0] == 0 ? '😊' : '😡')}{(i + 1) % 4 === 0 && (arr[i][0] == 0 ? '😊' : '😡')}</button>
+                    <button style={(i + 1) % 3 === 0 ? grad2 : (i + 1) % 4 === 0 ? greenbox : blackbox1} onClick={() => handaleclick(arr[i][1])}>{(((i + 1) % 3 === 0) && show) && (arr[i][1] == 0 ? '😊' : '😡')}{(i + 1) % 4 === 0 && (arr[i][1] == 0 ? '😊' : '😡')}</button>
+                    <button style={(i + 1) % 3 === 0 ? grad3 : (i + 1) % 4 === 0 ? greenbox : blackbox1} onClick={() => handaleclick(arr[i][2])}>{(((i + 1) % 3 === 0) && show) && (arr[i][2] == 0 ? '😊' : '😡')}{(i + 1) % 4 === 0 && (arr[i][2] == 0 ? '😊' : '😡')}</button>
+                    <button style={(i + 1) % 3 === 0 ? grad4 : (i + 1) % 4 === 0 ? greenbox : blackbox1}
+                      onClick={() => handaleclick(arr[i][3])}
+                    >{(((i + 1) % 3 === 0) && show) && (arr[i][3] == 0 ? '😊' : '😡')}{(i + 1) % 4 === 0 && (arr[i][3] == 0 ? '😊' : '😡')}</button>
 
                   </div>
                 )
               })
             }
 
- <h1 style={{textAlign:"center"}}>{gameOverMassage}</h1>
+            <h1 style={{ textAlign: "center" }}>{gameOverMassage}</h1>
           </div>
 
         </div>
